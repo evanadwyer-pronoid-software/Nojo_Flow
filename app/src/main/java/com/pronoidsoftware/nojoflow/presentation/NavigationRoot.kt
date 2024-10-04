93a61c23a3922d88fe57ffe6c9a252482a97544c
evanadwyer <evanadwyer@pronoid-software.com>
2024-10-04 14:08:24 -0700

    enter and exit transitions for navigating screens

--- app/src/main/java/com/pronoidsoftware/nojoflow/presentation/NavigationRoot.kt
+++ app/src/main/java/com/pronoidsoftware/nojoflow/presentation/NavigationRoot.kt
@@ -1,5 +1,7 @@
 package com.pronoidsoftware.nojoflow.presentation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -12,9 +14,22 @@ import kotlinx.serialization.Serializable
 fun NavigationRoot(navHostController: NavHostController) {
     NavHost(
         navController = navHostController,
-        startDestination = NoteList
+        startDestination = NoteList,
     ) {
-        composable<NoteList> {
+        composable<NoteList>(
+            enterTransition = {
+                slideIntoContainer(
+                    animationSpec = tween(1000),
+                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    animationSpec = tween(1000),
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                )
+            }
+        ) {
             NoteListScreenRoot(
                 onNavigateToEditNote = { id, requiredWritingTimeMin ->
                     navHostController.navigate(
@@ -26,7 +41,20 @@ fun NavigationRoot(navHostController: NavHostController) {
                 }
             )
         }
-        composable<EditNote> {
+        composable<EditNote>(
+            enterTransition = {
+                slideIntoContainer(
+                    animationSpec = tween(1000),
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    animationSpec = tween(1000),
+                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                )
+            }
+        ) {
             EditNoteScreenRoot(
                 onCancel = {
                     navHostController.navigateUp()
