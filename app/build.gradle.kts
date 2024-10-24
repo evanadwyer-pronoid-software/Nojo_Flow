5e4fcd1bf11b7007ca4143d371e65e02575a2918
evanadwyer <evanadwyer@pronoid-software.com>
2024-10-24 10:35:27 -0500

    upgrade for Android 15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,14 +10,14 @@ plugins {
 
 android {
     namespace = "com.pronoidsoftware.nojoflow"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pronoidsoftware.nojoflow"
         minSdk = 30
-        targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        targetSdk = 35
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
