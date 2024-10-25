2ad2d47f88632341aa725701aefce4ed7e933f19
evanadwyer <evanadwyer@pronoid-software.com>
2024-10-25 09:42:41 -0500

    default predictive back gesture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.pronoidsoftware.nojoflow"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
